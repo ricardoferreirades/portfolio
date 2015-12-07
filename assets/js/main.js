9e9f8a1f187b8b69c7e66e630fadc8e995adc6c1
Ricardo Ferreira <ricardoferreirades@gmail.com>
2015-12-07 00:18:24 -0300

    tornando o menu responsivo, modificando seu comportamento para adaptarse a janela. Inserindo as meta tags para viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
+	
+	// Contact form validation
 	$("#sendForm").validate();
 	
+	// navigation scroll
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
@@ -11,5 +14,24 @@ $(document).ready(function(){
 	        'scrollTop': $target.offset().top - 0
 	    }, 900, 'swing');
 	});
-	
+
+	// collapseble menu
+	$("#navBars").click(function(){
+		$(this).prop("disabled", true);
+		$("#menu").slideToggle(1000, function(){
+			$("#navBars").prop("disabled", false);
+		});
+	});
+
+	// Hiding the menu when the page loads
+	$("#menu").hide();
+
+	// showing the menu when the window changes
+	$(window).resize(function(){
+	    if($(window).width() > 767){
+	      $("#menu").show();
+	    }else{
+	      $("#menu").hide();
+	    }
+	});
 });
